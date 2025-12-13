e594b2ce62750aa39dcb365c55372802ba69f335
Palks studio <palksstudio@gmail.com>
2025-12-13 23:14:52 +0100

    Delete mock/js/script.js

--- mock/js/script.js
+++
@@ -1,9 +0,0 @@
-/* =========================================
-Anonymized JavaScript Example
-Palks Studio — Mock file
-========================================= */
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Mock script loaded — Palks Studio");
-});
-
